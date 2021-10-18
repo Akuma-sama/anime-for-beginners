9010ca9742f2e9447cc3c671e32b5c4c0386679e
kingSummit <87880680+Akuma-sama@users.noreply.github.com>
2021-10-18 14:06:09 -0400

    add custom cursor tracer

--- main.js
+++ main.js
@@ -5,3 +5,19 @@ menuToggle.addEventListener("click", () => {
     menuToggle.classList.toggle("active")
     showcase.classList.toggle("active")
 })
+
+
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
